7c0043acd686e5be12dac5b0e481e2c012ec5d07
Alexis Jamal <alexisjamal@hotmail.fr>
2026-03-19 10:14:01 +0100

    Test du workflow CI/CD

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = os.getenv('USER_NAME', 'Alexis')
-    return f"Bonjour {name} ! L'API fonctionne."
+    return f"Bonjour {name} ! L'API fonctionne bien."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
